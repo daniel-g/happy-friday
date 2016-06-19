c543b26500b9c70db6d72496fe65a227bdab1105
Daniel Gaytan <dgaytan6@gmail.com>
2016-06-19 04:11:27 -0500

    Spec hard case of report

--- spec/models/report_spec.rb
+++ spec/models/report_spec.rb
@@ -75,6 +75,27 @@ describe Report do
     end
   end
 
+  context 'many teams in different timezones: hard case' do
+    let!(:moscow){ FactoryGirl.create(:team, name: 'Moscow', timezone: 3, dev_performance: 1, qa_performance: 0.5) }
+    let!(:zagreb){ FactoryGirl.create(:team, name: 'Zagreb', timezone: 2, dev_performance: 0.25, qa_performance: 0.25) }
+    let!(:london){ FactoryGirl.create(:team, name: 'London', timezone: 0, dev_performance: 0.5, qa_performance: 1) }
+
+    let!(:moscow_1) { FactoryGirl.create(:task, team: moscow, dev_estimation: 2, qa_estimation: 1, external_id: 1) }
+    let!(:moscow_2) { FactoryGirl.create(:task, team: moscow, dev_estimation: 2, qa_estimation: 2, external_id: 4) }
+    let!(:london_1) { FactoryGirl.create(:task, team: london, dev_estimation: 2, qa_estimation: 1, external_id: 2) }
+    let!(:london_2) { FactoryGirl.create(:task, team: london, dev_estimation: 1, qa_estimation: 2, external_id: 3) }
+
+    subject(:data) { report.data }
+
+    it 'generates a report of tasks sorted by hour of work' do
+      expect(data.size).to eq(Task.count)
+      expect(data[0]).to eq(to_data_hash([moscow.name, '9:00am - 1:00pm', '6:00am - 10:00am', moscow_1.external_id]))
+      expect(data[1]).to eq(to_data_hash([london.name, '9:00am - 2:00pm', '9:00am - 2:00pm', london_1.external_id]))
+      expect(data[2]).to eq(to_data_hash([moscow.name, '1:00pm - 7:00pm', '10:00am - 4:00pm', moscow_2.external_id]))
+      expect(data[3]).to eq(to_data_hash([london.name, '2:00pm - 6:00pm', '2:00pm - 6:00pm', london_2.external_id]))
+    end
+  end
+
   it 'raises exception if the directory of the file specified does not exist' do
     missing_path = Framework.app.root.join('spec/fixtures/missing_folder/report.csv')
     expect{ Report.new(file_name: missing_path) }.to raise_error(RuntimeError)
